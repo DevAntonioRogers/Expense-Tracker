9743b6ed36492006ab9b27ac38d76e7f27c66dcd
DevAntonioRogers <hydrohs6@gmail.com>
2023-01-12 23:16:39 -0500

    Few Updates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,11 @@ function ExpenseItem(props) {
 
   return (
     <div className='expense-item'>
-      <div>{props.date.toString}</div>
+      <div>
+        <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div>
+        <div></div>
+        <div></div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
