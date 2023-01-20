f65e93290d02328692cb09a6081a92951ace029f
DevAntonioRogers <hydrohs6@gmail.com>
2023-01-20 01:36:22 -0500

    Added Dates

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css'
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const day = props.date.toLocaleString('en-us', { day: '2-digit' })
+  const year = props.date.getFullYear()
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date_month'>{month}</div>
+      <div className='expense-date_year'>{year}</div>
+      <div className='expense-date_dat'>{day}</div>
+    </div>
+  )
+}
+
+export default ExpenseDate
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 
 
 function ExpenseItem(props) {
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div>
-        <div></div>
-        <div></div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
